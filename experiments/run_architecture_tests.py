101fc404d6cd6c5a49fb415a282f35abc5cecec1
somesoo <jakasperczak@gmail.com>
2025-05-19 23:00:19 +0200

    script calculating best parameters

--- experiments/run_architecture_tests.py
+++
@@ -1,3 +0,0 @@
-def test_different_architectures():
-    # Testuj sieć z różną liczbą warstw i neuronów
-    pass
---
+++ run_architecture_tests.py
@@ -0,0 +1,63 @@
+import time
+import numpy as np
+
+from function import generate_data
+from neuron_network.mlp import MLP
+from neuron_network.optimizer import SGD
+from neuron_network.loss import mse, mse_derivative
+
+def optimize_architecture():
+    learning_rates = [0.7]
+    neuron_counts = [100, 20]
+    layer_counts = [1, 3]
+
+
+    results = []
+
+    # Dane wejściowe
+    X, y = generate_data(n_samples=100, range=(-10, 10), seed=42)
+    X = X.reshape(1, -1)
+    y = y.reshape(1, -1)
+
+    print("Start optymalizacji parametrów...\n")
+
+    for lr in learning_rates:
+        for num_layers in layer_counts:
+            for neurons in neuron_counts:
+                hidden_layers = [neurons] * num_layers
+
+                model = MLP([1] + hidden_layers + [1], activation="tanh")
+                optimizer = SGD(learning_rate=lr)
+                epochs = 20000
+                start = time.time()
+                for epoch in range(epochs):  # ustalona liczba epok
+                    y_pred = model.forward(X)
+                    loss = mse(y, y_pred)
+                    grad = mse_derivative(y, y_pred)
+                    model.backward(grad)
+                    optimizer.step(model.layers)
+                end = time.time()
+
+                results.append({
+                    "learning_rate": lr,
+                    "layers": num_layers,
+                    "neurons": neurons,
+                    "loss": loss,
+                    "time": end - start
+                })
+
+                print(f"{num_layers} warstw × {neurons} neurony | LR={lr:.2f} -> Loss={loss:.4f}, Time={end-start:.2f}s")
+
+    # Sortowanie: najpierw jakość, potem czas
+    best = sorted(results, key=lambda x: (x["loss"], x["time"]))[0]
+
+    print("\nNajlepsza konfiguracja:")
+    print(f" - Learning rate: {best['learning_rate']}")
+    print(f" - Liczba warstw: {best['layers']}")
+    print(f" - Neuronów w każdej warstwie: {best['neurons']}")
+    print(f" - Końcowy błąd (MSE): {best['loss']:.5f}")
+    print(f" - Czas treningu: {best['time']:.2f} s")
+
+    return best
+
+optimize_architecture()
